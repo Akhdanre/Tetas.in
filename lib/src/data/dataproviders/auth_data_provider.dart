b26f22657c806a464150be8b487d048656d911e2
Akhdanre <danakhdan12@gmail.com>
2023-11-29 23:37:40 +0700

    add url and change data profider name

--- lib/src/data/dataproviders/auth_data_provider.dart
+++ lib/src/data/dataproviders/auth_data_provider.dart
@@ -2,9 +2,9 @@ import 'package:http/http.dart' as http;
 import 'package:tetas_in/config/base_url.dart';
 import 'package:tetas_in/src/data/models/login_request.dart';
 
-class LoginAPI {
+class AuthDataProvider {
   Future<dynamic> login(LoginRequest data) {
-    Uri url = Uri.parse(BaseUrl.fullApiUrl);
+    Uri url = Uri.parse("${BaseUrl.fullApiUrl}/authentication");
     var response = http.post(url, body: data);
     return response;
   }
