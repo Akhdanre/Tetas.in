abd5b565191489eaa4fe4164e9edc513993e1f6c
Akhdanre <danakhdan12@gmail.com>
2023-12-02 05:19:40 +0700

    udpate login

--- lib/src/data/dataproviders/auth_data_provider.dart
+++ lib/src/data/dataproviders/auth_data_provider.dart
@@ -3,8 +3,9 @@ import 'package:tetas_in/src/data/models/login_request.dart';
 
 class AuthAPI {
   Future<dynamic> login(LoginRequest data) async {
-    Uri url = Uri.http("10.10.10.251:8000", "/api/authentication");
+    Uri url = Uri.http("192.168.1.13:8000", "/api/authentication");
     var response = await http.post(url, body: data);
-    return response;
+    if (response.statusCode == 200) return response;
+    return null;
   }
 }
--- lib/src/data/repositories/auth_repository.dart
+++ lib/src/data/repositories/auth_repository.dart
@@ -1,24 +1,24 @@
-// import 'package:tetas_in/src/data/dataproviders/auth_data_provider.dart';
-// import 'package:tetas_in/src/data/models/base_model_response.dart';
-// import 'package:tetas_in/src/data/models/login_request.dart';
-// import 'package:tetas_in/src/data/models/login_resposen.dart';
+import 'package:tetas_in/src/data/dataproviders/auth_data_provider.dart';
+import 'package:tetas_in/src/data/models/login_request.dart';
+import 'package:tetas_in/src/data/models/login_resposen.dart';
 
-// class AuthRepository {
-//   final AuthAPI authDataProvider = AuthAPI();
+class AuthRepository {
+  final AuthAPI authapi = AuthAPI();
 
-//   Future<LoginResponse> postLogin(String username, String password) async {
-//     try {
-//       LoginRequest user = LoginRequest(username: username, password: password);
-
-//       final BaseModelResponse response = await authDataProvider.login(user);
-//       if (response.data != null) {
-//         LoginResponse token = LoginResponse.fromJson(response.data);
-//         return token;
-//       }
-//       return LoginResponse(token: "");
-//     } catch (e) {
-//       return LoginResponse(token: "");
-
-//     }
-//   }
-// }
+  Future<LoginResponse> postLogin(String username, String password) async {
+    try {
+      LoginRequest user = LoginRequest(username: username, password: password);
+      print("before auth api");
+      final response = await authapi.login(user);
+      print("after auth api");
+      if (response != null) {
+        LoginResponse token = LoginResponse.fromJson(response.data);
+        return token;
+      }
+      return LoginResponse(token: "");
+    } catch (e) {
+      print(e);
+      return LoginResponse(token: "");
+    }
+  }
+}
