06ef583383825653fc4826d30f2ba1c13faf0213
Akeon <danakhdan12@gmail.com>
2024-01-02 02:06:49 +0700

    adjust view in start inku

--- lib/src/presentation/pages/start_inku_screen.dart
+++ lib/src/presentation/pages/start_inku_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tetas_in/src/presentation/widgets/app_bar_custom.dart';
 import 'package:tetas_in/src/presentation/widgets/background.dart';
 import 'package:tetas_in/src/presentation/widgets/text_field_custom_all.dart';
 import 'package:tetas_in/src/utils/color_string.dart';
 import 'package:tetas_in/src/utils/size_config.dart';
+
+import '../../utils/shared_preferences/user_data.dart';
 
 class StartInkuPage extends StatefulWidget {
   const StartInkuPage({super.key});
@@ -15,7 +18,19 @@ class StartInkuPage extends StatefulWidget {
 class _StartInkuPageState extends State<StartInkuPage> {
   TextEditingController idInku = TextEditingController();
   TextEditingController startData = TextEditingController();
-  TextEditingController numberofegg = TextEditingController();
+  TextEditingController numberOfEgg = TextEditingController();
+  late List<String> listInku;
+  String selectedValue = "INK0001";
+
+  @override
+  void initState() {
+    super.initState();
+    dataInku();
+  }
+
+  Future<void> dataInku() async {
+    listInku = await UserData().inkubator;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,10 +48,30 @@ class _StartInkuPageState extends State<StartInkuPage> {
                     const SizedBox(
                       height: 30,
                     ),
-                    TextFieldCustomAll(
-                        title: "ID Inkubators",
-                        textHint: "INK0001",
-                        controller: idInku),
+                    BlocBuilder(
+                      builder: (context, state) {
+                        if (1 == 1) {
+                          return DropdownButton<String>(
+                            value: selectedValue,
+                            underline: const SizedBox(),
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black),
+                            items: listInku
+                                .map((e) =>
+                                    DropdownMenuItem(value: e, child: Text(e)))
+                                .toList(),
+                            onChanged: (newValue) {
+                              // context.read<HomeBloc>().add(
+                              //     DataInkubatorSwitch(id: newValue!));
+                            },
+                          );
+                        }
+                        return TextFieldCustomAll(
+                            title: "Start Date",
+                            textHint: "02/08/2023",
+                            controller: startData);
+                      },
+                    ),
                     TextFieldCustomAll(
                         title: "Start Date",
                         textHint: "02/08/2023",
@@ -48,7 +83,7 @@ class _StartInkuPageState extends State<StartInkuPage> {
                     TextFieldCustomAll(
                         title: "Number Of Eggs",
                         textHint: "12",
-                        controller: numberofegg),
+                        controller: numberOfEgg),
                     const Spacer(),
                     ElevatedButton(
                         onPressed: () {},
